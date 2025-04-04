e69d5b94cf11a629bbc61b754f37bee77d58fc02
Jonh Wasny <125569896+Wasny-loop@users.noreply.github.com>
2025-04-04 11:11:08 -0300

    adição das datas

--- sla.java
+++ sla.java
@@ -1,7 +1,7 @@
 public static void main(String args[]){
     String nome  
-    int anoAtual 
-    int dataNasc 
+    int anoAtual = 2025;
+    int dataNasc = 2003;
 
 int idade = anoAtual-dataNasc
     System.out.println("Sua idade Wasny, é: ", idade)
