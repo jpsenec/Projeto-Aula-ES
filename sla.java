08a15124ddb93ef437ed61f58a66e444b6da3b13
João Pedro <126182719+jpsenec@users.noreply.github.com>
2025-04-04 11:08:40 -0300

    dale

---
+++ sla.java
@@ -0,0 +1,10 @@
+public static void main(String args[]){
+    String nome  
+    int anoAtual 
+    int dataNasc 
+
+int idade = anoAtual-dataNasc
+    System.out.println("Sua idade Wasny, é: ", idade)
+
+
+}
